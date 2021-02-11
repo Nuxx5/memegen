64973bbd9c17be619408d58dac79c09a6861bee3
Ran Porath <ranporath@gmail.com>
2021-02-11 15:53:36 +0200

    added canvas

--- js/meme-controller.js
+++ js/meme-controller.js
@@ -1,6 +1,27 @@
 'use strict'
 
+var gElCanvas;
+var gCtx;
+
 function onInit() {
-    renderImages()
+    
 }
 
+function drawImage(image) {
+  gMeme.selectedImgId = image.id;
+  document.querySelector('section.gallery').classList.add('hidden');
+  document.querySelector('.about').classList.add('hidden');
+  document.querySelector('section.main-canvas').classList.remove('hidden');
+  gElCanvas = document.getElementById('my-canvas')
+  gCtx = gElCanvas.getContext('2d');
+  gCtx.drawImage(image, 0, 0, gElCanvas.width, gElCanvas.height);
+}
+
+function showAbout(){
+    document.querySelector('.about').classList.remove('hidden');
+    document.querySelector('section.gallery').classList.add('hidden');
+}
+
+function showGallery(){
+    document.querySelector('section.gallery').classList.remove('hidden');
+}
